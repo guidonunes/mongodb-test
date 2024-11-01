c59017b38a47931c41f62c7547a01ddce67e6226
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-01 16:20:27 -0300

    create database table

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test')
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
+
+
+  //Schema
+  //Document, Collection, Database
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  isMarried: Boolean,
+  salary: Number,
+  gender: String
+});
+
+const User = mongoose.model('User', userSchema);
